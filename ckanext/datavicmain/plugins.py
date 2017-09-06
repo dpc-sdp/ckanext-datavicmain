628226db0dfe7ce088298b3c5c1641f86a3a494d
Nathan Perry <nathan@salsadigital.com.au>
2017-09-06 18:48:37 +1200

    DATAVICIAR-4: Remove unused demo code from hello world customisation example.

--- ckanext/datavicmain/plugins.py
+++ ckanext/datavicmain/plugins.py
@@ -26,11 +26,9 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IDatasetForm, inherit=True)
     p.implements(p.IPackageController, inherit=True)
-    p.implements(p.IResourceController, inherit=True)
+    # p.implements(p.IResourceController, inherit=True)
 
     ## helper methods ## 
-
-    MUSIC_GENRES = ['classical', 'rock', 'pop', 'heavy-metal', 'jazz', 'ethnic',]
 
     YES_NO_OPTIONS = ['yes', 'no',]
 
@@ -38,7 +36,8 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
         ('extract', {'label': 'Extract'}),
         ('location', { 'label': 'Location'}),
         ('date_creation_acquisition', {'label': 'Date of Creation or Acquisition'}),
-        ('last_updated', {'label': 'Last Updated'}),
+        # Last updated is a core field..
+        # ('last_updated', {'label': 'Last Updated'}),
         ('update_frequency', {'label': 'Update Frequency'}),
         ('public_release_date', {'label': 'Public Release Date'}),
         ('geographic_coverage', {'label': 'Geographic - Coverage'}),
@@ -100,53 +99,10 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
     ]
 
     @classmethod
-    def create_music_genres(cls):
-        '''Create music genres vocabulary and tags, if they don't exist already.
-        Note that you could also create the vocab and tags using CKAN's api,
-        and once they are created you can edit them (add or remove items) using the api.
-        '''
-        user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
-        context = {'user': user['name']}
-        try:
-            data = {'id': 'music_genres'}
-            toolkit.get_action ('vocabulary_show') (context, data)
-            log1.info("The music-genres vocabulary already exists. Skipping.")
-        except toolkit.ObjectNotFound:
-            log1.info("Creating vocab 'music_genres'")
-            data = {'name': 'music_genres'}
-            vocab = toolkit.get_action ('vocabulary_create') (context, data)
-            for tag in cls.MUSIC_GENRES:
-                log1.info("Adding tag {0} to vocab 'music_genres'".format(tag))
-                data = {'name': tag, 'vocabulary_id': vocab['id']}
-                toolkit.get_action ('tag_create') (context, data)
-
-    @classmethod
-    def music_genres(cls):
-        '''Return the list of all existing genres from the music_genres vocabulary.'''
-        cls.create_music_genres()
-        try:
-            music_genres = toolkit.get_action ('tag_list') (data_dict={ 'vocabulary_id': 'music_genres'})
-            return music_genres
-        except toolkit.ObjectNotFound:
-            return None
-
-    @classmethod
-    def music_genres_options(cls):
-        ''' This generator method is only usefull for creating select boxes. '''
-        for name in cls.music_genres():
-            yield { 'value': name, 'text': name }
-
-    @classmethod
     def yes_no_options(cls):
         ''' This generator method is only usefull for creating select boxes. '''
         for option in cls.YES_NO_OPTIONS:
             yield { 'value': option, 'text': option.capitalize() }
-
-    @classmethod
-    def hello_world(cls):
-        ''' This is our simple helper function. '''
-        html = '<span>Hello World</span>'
-        return p.toolkit.literal(html)
 
     @classmethod
     def organization_list_objects(cls, org_names = []):
@@ -182,10 +138,6 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
         These helpers will be available under the 'h' thread-local global object.
         '''
         return {
-            # define externsion-specific helpers
-            'hello_world': self.hello_world,
-            'music_genres': self.music_genres,
-            'music_genres_options': self.music_genres_options,
             'organization_list_objects': self.organization_list_objects,
             'organization_dict_objects': self.organization_dict_objects,
             'dataset_extra_fields': self.DATASET_EXTRA_FIELDS,
@@ -230,25 +182,7 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
 
         from ckan.lib.navl.dictization_functions import missing, StopOnError, Invalid
 
-        def music_title_converter_1(key, data, errors, context):
-            ''' Demo of a typical behaviour inside a validator/converter '''
-
-            ## Stop processing on this key and signal the validator with another error (an instance of Invalid) 
-            #raise Invalid('The music title (%s) is invalid' %(data.get(key,'<none>')))
-
-            ## Stop further processing on this key, but not an error
-            #raise StopOnError
-            pass
-
-        def music_title_converter_2(value, context):
-            ''' Demo of another style of validator/converter. The return value is considered as 
-            the converted value for this field. '''
-            
-            #raise Exception ('Breakpoint music_title_converter_2')
-            #raise Invalid('The music title is malformed')
-            from string import capitalize
-            return capitalize(value)
-
+        # DataVic: Helper function for adding extra dataset fields
         def append_field(extras_list, data, key):
             items = filter(lambda t: t['key'] == key, extras_list)
             if items:
@@ -272,13 +206,7 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
             else:
                 extras_list.append({ 'key': 'record_modified_at', 'value': datestamp })
 
-            # Note Append "foo.x1" field as dynamic (not registered under modify schema) field
-            #items = filter(lambda t: t['key'] == 'foo.x1', extras_list)
-            #if items:
-            #    items[0]['value'] = data.get(('foo.x1',))
-            #else:
-            #    extras_list.append({ 'key': 'foo.x1', 'value': data.get(('foo.x1',)) })
-
+            # DataVic: Append extra fields as dynamic (not registered under modify schema) field
             for field in self.DATASET_EXTRA_FIELDS:
                 append_field(extras_list, data, field[0])
 
@@ -289,37 +217,10 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
             # we take into account. If we omitted this step, the ('__extras',) item would have 
             # been lost (along with the POSTed value). 
             if context.get('package'):
-                # The package is created (at least as draft)
-                #data[('foo.x1',)] = data[('__extras',)].get('foo.x1')
-
-                # Add extra fields..
+                # DataVic: Add extra fields..
                 for field in self.DATASET_EXTRA_FIELDS:
                     data[(field[0],)] = data[('__extras',)].get(field[0])
             pass
-
-        # Update default validation schema (inherited from DefaultDatasetForm)
-
-        schema.update({
-            # Add our custom "music_genre" metadata field to the schema.
-            'music_genre': [
-                toolkit.get_validator('ignore_missing'),
-                toolkit.get_converter('convert_to_tags')('music_genres'),
-            ],
-            # Add our "music_title" metadata field to the schema, this one will use
-            # convert_to_extras instead of convert_to_tags.
-            'music_title': [
-                toolkit.get_validator('ignore_missing'),
-                music_title_converter_1,
-                music_title_converter_2,
-                toolkit.get_converter('convert_to_extras'),
-            ],
-            # Note We do not explicitly declare "foo.x1" schema, because we'll add it 
-            # dynamically at before_validation_processor.
-            #'foo.x1': [
-            #    toolkit.get_validator('ignore_missing'),
-            #    toolkit.get_converter('convert_to_extras'), 
-            #]
-        })
 
         # Add our custom_resource_text metadata field to the schema
         # schema['resources'].update({
@@ -365,16 +266,6 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
 
         # Create a dictionary containing the extra fields..
         dict_extra_fields = {
-            # Add our custom "music_genre" metadata field to the schema.
-            'music_genre': [
-                toolkit.get_converter('convert_from_tags')('music_genres'),
-                toolkit.get_validator('ignore_missing')
-            ],
-            # Add our "music_title" field to the dataset schema.
-            'music_title': [
-                toolkit.get_converter('convert_from_extras'),
-                toolkit.get_validator('ignore_missing')
-            ],
             # Add our non-input field (created at after_validation_processor)
             'record_modified_at': [
                 toolkit.get_converter('convert_from_extras'),
@@ -507,14 +398,14 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
         
         # add some extras to the 2-column table
         
-        dt = pkg_dict.get('dataset_type') 
+        dt = pkg_dict.get('dataset_type')
 
         extras = pkg_dict.get('extras', [])
         if not extras:
             pkg_dict['extras'] = extras
 
-        extras.append({ 'key': 'Music Title', 'value': pkg_dict.get('music_title', 'n/a') })
-        extras.append({ 'key': 'Music Genre', 'value': pkg_dict.get('music_genre', 'n/a') })
+        # extras.append({ 'key': 'Music Title', 'value': pkg_dict.get('music_title', 'n/a') })
+        # extras.append({ 'key': 'Music Genre', 'value': pkg_dict.get('music_genre', 'n/a') })
 
         # or we can translate keys ...
         
@@ -527,5 +418,4 @@ class DatasetForm(p.SingletonPlugin, toolkit.DefaultDatasetForm):
             item['key'] = field_key_map.get(k, k)
         
         return pkg_dict
-
 
