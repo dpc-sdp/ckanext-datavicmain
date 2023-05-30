b354777bebb47855c12457e909958d2e1fc86be1
mutantsan <kvaqich@gmail.com>
2023-05-30 10:17:59 +0300

    SXDEDPCXZIC-164 / Save data record before adding data resources

--- ckanext/datavicmain/validators.py
+++ ckanext/datavicmain/validators.py
@@ -6,14 +6,11 @@ log = logging.getLogger(__name__)
 
 
 def datavic_tag_string(key, data, errors, context):
-
-    request = toolkit.request if repr(toolkit.request) != '<LocalProxy unbound>' and hasattr(toolkit.request, 'params') else None
-
+    request = toolkit.request if repr(toolkit.request) != '<LocalProxy unbound>' and hasattr(toolkit.request, 'args') else None
     if request:
         end_point = toolkit.get_endpoint()
-        if end_point and end_point[0] == 'dataset' and end_point[1] in ['new', 'edit']:
+        if end_point and end_point[0] in ["dataset", "datavic_dataset"] and end_point[1] in ['new', 'edit']:
             toolkit.get_validator('not_empty')(key, data, errors, context)
             return
 
     toolkit.get_validator('ignore_missing')(key, data, errors, context)
-
