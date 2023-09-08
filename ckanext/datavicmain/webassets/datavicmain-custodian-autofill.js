947ebe24cf9cfe2e41dc790fee857db3e073adf3
oleksandrivaniuk <oleksandr.ivaniuk@linkdigital.com.au>
2023-09-08 17:23:56 +0300

    SXDEDPCXZIC-224 / update autofill custodian details during dataset creation

--- ckanext/datavicmain/webassets/datavicmain-custodian-autofill.js
+++ ckanext/datavicmain/webassets/datavicmain-custodian-autofill.js
@@ -1,23 +1,13 @@
 this.ckan.module('datavicmain-custodian-autofill', function ($) {
     'use strict';
-    let userCache = null;
     return {
         options: {
-            property: null,
+            "default_value": null,
         },
         initialize: function () {
             if ($('.stage-1')[0] && !this.el.val()) {
-                this._fillField(this)
+                this.el.val(this.options.default_value)
             }
         },
-        _fillField: async function (e) {
-            if (!userCache) {
-                userCache = new Promise((ok, fail) => {
-                    fetch(this.sandbox.client.call("GET", "user_show", "", data => ok(data.result), fail));
-                });
-            };
-            const user = await userCache;
-            this.el.val(user[this.options.property])
-        }
     };
 });
