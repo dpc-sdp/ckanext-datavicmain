785185a202b50dc195955098d819d66a9d37a9b4
agmorev <agmorev@gmail.com>
2024-04-15 19:31:54 +0300

    SXDEDPCXZIC-295 / fix access to user activity stream

--- ckanext/datavicmain/logic/auth.py
+++ ckanext/datavicmain/logic/auth.py
@@ -1,5 +1,8 @@
 import ckan.plugins.toolkit as tk
 
+from ckan import authz
+from ckan.types import Context, DataDict, AuthResult
+
 from ckanext.datavicmain import helpers, const
 
 
@@ -44,4 +47,32 @@ def package_update(next_auth, context, data_dict):
 
 
 def datavic_toggle_organization_uploads(context, data_dict):
+    return {"success": False}
+
+
+def user_show(context: Context, data_dict: DataDict) -> AuthResult:
+    if tk.request and (
+        tk.get_endpoint() == ("datavicuser", "perform_reset") or
+        tk.get_endpoint() == ("activity", "user_activity")
+    ):
+        return {"success": True}
+    user_id = authz.get_user_id_for_username(data_dict.get("id"))
+    is_myself = tk.current_user.name == data_dict.get("id")
+    is_sysadmin = authz.is_sysadmin(tk.current_user.name)
+
+    if is_sysadmin or is_myself:
+        return {"success": True}
+
+    orgs = tk.get_action("organization_list_for_user")(
+        {"user": tk.current_user.name}, {"permission": "admin"}
+    )
+    for org in orgs:
+        members = tk.get_action("member_list")(
+            {}, {"id": org.get("id"), "object_type": "user"}
+        )
+        member_ids = [member[0] for member in members]
+
+        if user_id in member_ids:
+            return {"success": True}
+
     return {"success": False}
