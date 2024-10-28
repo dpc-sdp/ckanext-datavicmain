b29c7f500aae3dd3ab43890d3b524085d6fcf77a
Yan Rudenko <yan.rudenko@linkdigital.com.au>
2024-10-28 09:29:10 +0100

    SXDEDPCXZIC-388 / Rework solution for sync logos

--- ckanext/datavicmain/logic/action.py
+++ ckanext/datavicmain/logic/action.py
@@ -6,6 +6,7 @@ from typing import Any, OrderedDict
 import ckanapi
 from sqlalchemy import or_
 
+from ckan.lib import uploader
 import ckan.lib.plugins as lib_plugins
 import ckan.plugins.toolkit as toolkit
 from ckan import model
@@ -183,13 +184,19 @@ def organization_update(next_, context, data_dict):
         patch = {f: result[f] for f in tracked_fields if f in result}
 
         if 'image_url' in tracked_fields and result.get('image_display_url'):
-            # Save the image as an full path URL instead of uploading to ODP.
-            patch['image_url'] = result['image_display_url']
+            grp_uloader: uploader.PUploader = uploader.get_uploader('group')
+            file_data = None
+            with open(grp_uloader.storage_path + '/' + result['image_url'], 'rb') as f:
+                file_data = f.read()
 
-        ckan.action.organization_patch(
-            id=remote["id"],
-            **patch,
-        )
+            patch['id'] = remote['id']
+            ckan.call_action('organization_patch', data_dict=patch, files={
+                "image_upload": (result['image_url'], file_data)})
+        else:
+            ckan.action.organization_patch(
+                id=remote["id"],
+                **patch,
+            )
 
     return result
 
